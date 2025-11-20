539f5e1c3b1fad5fbda1e1b9fcfc1505c16f072d
aya saad <ayasaadpr@gmail.com>
2025-11-20 20:54:43 +0100

    modification de homepage image

--- CRM/frontend/src/pages/HomePage.jsx
+++ CRM/frontend/src/pages/HomePage.jsx
@@ -1,16 +1,20 @@
 import { Link } from "react-router-dom";
 import "./HomePage.css";
+import image1 from "../assets/image1.png";
 
 export default function Home() {
   return (
     <div className="home-container">
-
       {/* Navbar */}
       <header className="navbar">
         <h1 className="logo">Mini CRM</h1>
         <nav className="nav-links">
-          <Link to="/login" className="nav-button">Log In</Link>
-          <Link to="/register" className="nav-button">Sign Up</Link>
+          <Link to="/login" className="nav-button">
+            Log In
+          </Link>
+          <Link to="/register" className="nav-button">
+            Sign Up
+          </Link>
         </nav>
       </header>
 
@@ -26,15 +30,22 @@ export default function Home() {
             Manage your clients, projects, and tasks all in one place.
           </p>
           <div className="buttons fade-in delay-400">
-            <Link to="/login" className="main-button">Log In</Link>
-            <Link to="/register" className="main-button">Sign Up</Link>
+            <Link to="/login" className="main-button">
+              Log In
+            </Link>
+            <Link to="/register" className="main-button">
+              Sign Up
+            </Link>
           </div>
         </div>
 
         {/* Right side image */}
-        
-          <img src="./image1.png" alt="CRM Illustration" className="main-image fade-in delay-400" />
-        
+
+        <img
+          src={image1}
+          alt="CRM Illustration"
+          className="main-image fade-in delay-400"
+        />
       </main>
 
       {/* Footer */}
